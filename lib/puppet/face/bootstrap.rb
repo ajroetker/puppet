126fd0467daaaf24881de031a573aa03ec01787d
Barr Iserloth <barr.iserloth@puppet.com>
2017-08-31 14:17:52 -0700

    (PE-21673) Modularize & package puppet-agent-bootstrap
    This commit moves the puppet-agent-bootstrap code created by Adrien
    Thebo (https://github.com/adrienthebo/puppet-agent-bootstrap/) to the
    puppetlabs github space, and adds rspec tests to go along with the
    bootstrap changes.

---
+++ lib/puppet/face/bootstrap.rb
@@ -0,0 +1,90 @@
+require 'puppet/face'
+require 'puppet/ssl'
+
+module PuppetX
+  module Puppetlabs
+    module Bootstrap
+      def verify_signed_cert!(hostcert)
+        if !hostcert.nil?
+          Puppet.notice "Found a certificate for #{Puppet[:certname]}"
+        else
+          Puppet.err "No signed certificate found for #{Puppet[:certname]}"
+          exit(1)
+        end
+      end
+
+      def verify_ssl_files_match!(hostcert, hostkey)
+        if hostcert.content.check_private_key(hostkey.content)
+          Puppet.notice "Private key matches certificate"
+        else
+          Puppet.err "Signed certificate does not match host private key"
+          exit(1)
+        end
+      end
+
+      def verify_node_definition_reachable!
+        Puppet::Node.indirection.find(Puppet[:node_name_value],
+                                      environment: Puppet::Node::Environment.remote('production'),
+                                      ignore_cache: true,
+                                      fail_on_404: true)
+        Puppet.notice("Contacted Puppet master for node definition")
+      rescue => e
+        Puppet.err "Unable to reach Puppet master: #{e.message}"
+        exit(1)
+      end
+    end
+  end
+end
+
+Puppet::Face.define(:bootstrap, '0.1.0') do
+  copyright "Puppet", 2017
+  summary "Initialize the Puppet agent"
+
+  action(:csr) do
+    summary "Initialize the agent key pair and save a CSR"
+
+    when_invoked do |opts|
+      Puppet::SSL::Oids.register_puppet_oids
+      Puppet::SSL::Oids.load_custom_oid_file(Puppet[:trusted_oid_mapping_file])
+
+      Puppet::SSL::Host.localhost
+    end
+  end
+
+  action(:purge) do
+    summary "Purge all agent SSL files"
+    when_invoked do |opts|
+      Puppet.notice("Purging CA CRL")
+      Puppet::SSL::CertificateRevocationList.indirection.destroy('ca')
+
+      Puppet.notice("Purging agent certificate")
+      Puppet::SSL::Certificate.indirection.destroy(Puppet[:certname])
+      Puppet.notice("Purging agent certificate request")
+      Puppet::SSL::CertificateRequest.indirection.destroy(Puppet[:certname])
+      Puppet.notice("Purging agent key pair")
+      Puppet::SSL::Key.indirection.destroy(Puppet[:certname])
+
+      nil
+    end
+  end
+
+  action(:verify) do
+    summary "Verify that the Puppet agent has a signed certificate"
+
+    when_invoked do |opts|
+      extend PuppetX::Puppetlabs::Bootstrap
+      hostcert = Puppet::SSL::Certificate.indirection.find(Puppet[:certname])
+      hostkey = Puppet::SSL::Key.indirection.find(Puppet[:certname])
+
+      verify_signed_cert!(hostcert)
+      verify_ssl_files_match!(hostcert, hostkey)
+      verify_node_definition_reachable!
+
+      hostcert
+    end
+
+    when_rendering :console do |cert|
+      cert.content.to_s if cert
+    end
+  end
+end
---
+++ spec/unit/face/bootstrap_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'puppet_spec/files'
+
+require 'puppet/face'
+
+describe Puppet::Face[:bootstrap, '0.1.0'] do
+  include PuppetSpec::Files
+
+  let(:bootstrap) { Puppet::Face[:bootstrap, '0.1.0'] }
+  let(:certificate) { Puppet::Face[:certificate, '0.0.1'] }
+  let(:hostname) { Puppet[:certname] }
+  let(:options) { {:ca_location => 'local'} }
+  let(:different_hostname) { hostname + 'different' }
+
+  before :each do
+    Puppet[:confdir] = tmpdir('conf')
+    Puppet::SSL::CertificateAuthority.stubs(:ca?).returns true
+
+    Puppet::SSL::Host.ca_location = :local
+
+    # We can't cache the CA between tests, because each one has its own SSL dir.
+    ca = Puppet::SSL::CertificateAuthority.new
+    Puppet::SSL::CertificateAuthority.stubs(:new).returns ca
+    Puppet::SSL::CertificateAuthority.stubs(:instance).returns ca
+  end
+
+  context "csr" do
+    it "initializes agent key pair and saves a CSR" do
+      # Because we're the CA, calling bootstrap csr will autosign the cert_request that bootstrap csr generates
+      expect { bootstrap.csr()  }.to_not raise_error
+      expect { bootstrap.verify()  }.to_not raise_error
+    end
+  end
+
+  context "verify" do
+    it "verifies an agent has a signed cert" do
+      certificate.generate(hostname, options)
+      certificate.sign(hostname, options)
+      expect { bootstrap.verify() }.to_not raise_error
+    end
+  end
+
+  context "purge" do
+    it "purges correctly" do
+      certificate.generate(different_hostname, options)
+      certificate.sign(different_hostname, options)
+      expect { bootstrap.verify() }.to raise_error(SystemExit)
+
+      expect { bootstrap.purge() }.to_not raise_error
+
+      certificate.generate(hostname, options)
+      certificate.sign(hostname, options)
+      expect { bootstrap.verify() }.to_not raise_error
+    end
+  end
+end
